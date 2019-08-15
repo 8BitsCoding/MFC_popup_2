223b37fc885f9b8b23b40545348d31a44175aeed
taehyung77@naver.com <taehyung77@naver.com>
2019-08-15 22:55:43 +0900

    commit

---
+++ MFCApplication1/MFCApplication1Dlg.cpp
@@ -0,0 +1,128 @@
+﻿
+// MFCApplication1Dlg.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "MFCApplication1.h"
+#include "MFCApplication1Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication1Dlg 대화 상자
+
+
+
+CMFCApplication1Dlg::CMFCApplication1Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION1_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_RBUTTONUP()
+	ON_COMMAND(20000, OnMyCommand)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication1Dlg 메시지 처리기
+
+BOOL CMFCApplication1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCApplication1Dlg::OnPaint()
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+	if (IsIconic())
+	{
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		// CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCApplication1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCApplication1Dlg::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	CMenu menu;
+	menu.CreatePopupMenu();
+	CString str;
+	for (int i = 0; i < 5; i++) {
+		str.Format(L"%d번 메뉴", i);
+		menu.AppendMenu(MF_STRING, 20000 + i, str);	// WM_COMMAND가 발새하고 wParam에서 메시지번호를 확인해야함.
+	}
+	
+	CPoint pos;
+	GetCursorPos(&pos);
+	menu.TrackPopupMenu(TPM_LEFTALIGN, pos.x, pos.y, this);
+	menu.DestroyMenu();
+
+	CDialogEx::OnRButtonUp(nFlags, point);
+}
+
+
+BOOL CMFCApplication1Dlg::OnCommand(WPARAM wParam, LPARAM lParam)
+{
+	int cmd_id = LOWORD(wParam);
+
+	if (cmd_id > 20000 && cmd_id < 20005) {
+		CString str;
+		str.Format(L"%d 항목을 선택했습니다.", cmd_id - 20000);
+		AfxMessageBox(str);
+	}
+
+	return CDialogEx::OnCommand(wParam, lParam);
+}
+
+void CMFCApplication1Dlg::OnMyCommand()
+{
+	// 20000 메시지 처리
+	AfxMessageBox(L"첫 번째 항목을 선택");
+}
